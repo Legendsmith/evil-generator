b506ceb50d767624004e68a6c036b18a7e8e9098
Legendsmith <legendsmith.au@gmail.com>
2019-09-24 17:51:57 +1000

    JSON loading refactor
    Refactored the code to actually load data properly and not use that hacky <script> tags in the HTML.
    Also added powers data, to be used in further updates.

--- main.js
+++ main.js
@@ -1,8 +1,100 @@
-//setup
+class Data{
+	constructor(){
+		this.realmData={};
+		this.domainsData={};
+		this.powersData={};
+		this.weaponsData={};
+		this.titlesData={};
+		this.load()
+	}
+	loadrealms(callback){
+		let realmshttp = new XMLHttpRequest();
+		realmshttp.onreadystatechange=function(self){
+			if (this.readyState== 4 && this.status==200){
+				callback(JSON.parse(realmshttp.responseText));
+				
+			}
+		}
+		realmshttp.open("GET","data/realms.json",true);
+		realmshttp.send();
+	}
+	loaddomains(callback){
+		let domainshttp = new XMLHttpRequest();
+		domainshttp.onreadystatechange=function(self){
+			if (this.readyState== 4 && this.status==200){
+				callback(JSON.parse(domainshttp.responseText));
+				
+			}
+		}
+		domainshttp.open("GET","data/domains.json",true);
+		domainshttp.send();
+	}
+	loadpowers(callback){
+		let xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function(self) {
+		  if (this.readyState == 4 && this.status == 200) {
+		    callback(JSON.parse(xmlhttp.responseText));
+		    
+		  }
+		};
+		xmlhttp.open("GET", "data/powers.json", true);
+		xmlhttp.send(); 
+	}
+	loadweapons(callback){
+		let xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function(self) {
+		  if (this.readyState == 4 && this.status == 200) {
+		    callback(JSON.parse(xmlhttp.responseText));
+		    
+		  }
+		};
+		xmlhttp.open("GET", "data/weapons.json", true);
+		xmlhttp.send(); 
+	}
+	loadtitles(callback){
+		let xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function(self) {
+		  if (this.readyState == 4 && this.status == 200) {
+		    callback(JSON.parse(xmlhttp.responseText));
+		    
+		  }
+		};
+		xmlhttp.open("GET", "data/titles.json", true);
+		xmlhttp.send(); 
+	}
+	load(){
+		console.log("Begin loading")
+		self = this
+		this.loadrealms(function(response){
+			self.realmData=response;
+			console.log(self.realmData)
+		});
+		this.loaddomains(function(response){
+			self.domainsData=response;
+			console.log("loaded domains")
+		})
+		this.loadpowers(function(response){
+			self.powersData=response;
+			console.log("loaded powers")
+		})
+		this.loadtitles(function(response){
+			self.titlesData=response;
+			console.log("loaded titles")
+		})
+		this.loadweapons(function(response){
+			self.weaponsData=response;
+			console.log("loaded weapons")
+		})
+		console.log("Finish loading")
+	}
 
+	
+}
 
 //btn setup
 btn_generate.onclick=generate;
+data = new Data()
+
 function getId(str){
   return document.getElementById(str)
 }
@@ -31,34 +123,34 @@ function generate(){
 	var namex = donames()
 	var name = namex[0]
 	var gender = namex[1].toLowerCase()
-	var _titlemaintypeR= randum(titletypes.main)
+	var _titlemaintypeR= randum(data.titlesData.titletypes.main)
 	var _titlemaintype = _titlemaintypeR //title type raw
 	var _titlemain = ""
-	_titlemain = randum(titles[_titlemaintype])
-	var _realmtype= randum(realmtypes)
-	var _realm = randum(realms[_realmtype])
+	_titlemain = randum(data.titlesData.titles[_titlemaintype])
+	var _realmtype= randum(data.realmData.realmtypes)
+	var _realm = randum(data.realmData.realms[_realmtype])
 	var genAdj=""
 	var power = _titlemain.power
 	if (power == 1){
 		power=2;
 	};
 	if(randumInt(10)<4){
-		genAdj = randum(titleAdj)
+		genAdj = randum(data.titlesData.titleAdj)
 	}
 	var outtext =`${genAdj}${_titlemain["o"] || _titlemain[gender]} of ${_realm}.`
 	var antistall=0
-	var realmlist = realms[_realmtype].slice(0)
+	var realmlist = data.realmData.realms[_realmtype].slice(0)
 	var titlelist = []
 	while (power>1){
-		var titleNextType = randum(titletypes.secondary)
-		var titleNext = randum(titles[titleNextType])
+		var titleNextType = randum(data.titlesData.titletypes.secondary)
+		var titleNext = randum(data.titlesData.titles[titleNextType])
 		var i = randumInt(realmlist.length)
 		var realmNext = realmlist[i]
 		realmlist.splice(i,1)
 		var genAdjN=""
 		if(titleNext.power <= power && realmNext!=_realm && realmlist.length >1){
 				if(randumInt(10)<3){
-				genAdjN = randum(titleAdj)
+				genAdjN = randum(data.titlesData.titleAdj)
 				}
 				titlelist.push(`${genAdjN}${titleNext["o"] || titleNext[gender]} of ${realmNext}`)
 				power -= titleNext.power
@@ -82,15 +174,14 @@ function generate(){
 
 	//Weapon
 	var vowelx = new RegExp('[aeiouAEIOU]')
-	var genweapon =  `${(randumInt(4)>2)?randum(meleewepdefs[_realmtype+("_wepAdj")])+" ":""}${randum(meleewepdefs.origin)} ${randum(meleewepdefs["generic_subtype"])} ${randum(meleewepdefs.wepType)} of ${randum(realms[_realmtype])}`
+	var genweapon =  `${(randumInt(4)>2)?randum(data.weaponsData[_realmtype+("_wepAdj")])+" ":""}${randum(data.weaponsData.origin)} ${randum(data.weaponsData["generic_subtype"])} ${randum(data.weaponsData.wepType)} of ${randum(data.realmData.realms[_realmtype])}`
 	outtext += `\n	${(gender=='m')?"He":"She"} ${p("wields|holds|brandishes|uses|possesses")} ${(vowelx.test(genweapon[0]))?"an":"a"} ${genweapon}.`
 	//domain
-	var maindomain = randum(domainsData.domains)
+	var maindomain = randum(data.domainsData.domains)
 	var domaintext = ""
 	var adjectives= new Array(randumInt(2));
-	//if(maindomain.ofOnly || randumInt(2)>0)
-	var place = domainsData.domain[randum(domainsData.domains)]
-	var noun = domainsData.domain[randum(domainsData.domains)]
+	var place = data.domainsData.domain[randum(data.domainsData.domains)]
+	var noun = data.domainsData.domain[randum(data.domainsData.domains)]
 	var descriptor = randum(place.descriptorclass)
 	var placeWord= randum(place.place)
 	var nounWord= randum(noun.noun)
@@ -106,10 +197,10 @@ function generate(){
 			domaintext=`the ${randum(place.adjective)} ${noun.topleveladj} ${placeWord} of ${nounWord}`
 		break;
 		case 3:
-			domaintext= `the ${randum(place.adjective)} ${noun.topleveladj} ${placeWord} of ${randum(domainsData.descriptors[descriptor])} ${nounWord}`
+			domaintext= `the ${randum(place.adjective)} ${noun.topleveladj} ${placeWord} of ${randum(data.domainsData.descriptors[descriptor])} ${nounWord}`
 		break;
 		case 4:
-			domaintext= `the ${placeWord} of ${randum(domainsData.descriptors[descriptor])} ${nounWord}`
+			domaintext= `the ${placeWord} of ${randum(data.domainsData.descriptors[descriptor])} ${nounWord}`
 		break;
 		case 5:
 			domaintext= `the ${placeWord} of ${noun.topleveladj} ${nounWord}`
@@ -120,13 +211,13 @@ function generate(){
 
 	//Output
 	getId("ta_output").value = getId("ta_output").value + `You have unleashed ${name} `  + outtext + "\n"
-	getId("h3_header").innerHTML = `${randum(headertxt)} ${name} ${headertext}`
+	getId("h3_header").innerHTML = `${randum(data.titlesData.headertxt)} ${name} ${headertext}`
 	getId("ta_output").scrollTop = getId("ta_output").scrollHeight;
 }
 
 function gentitle(_type,realm){
 	var txt = ""
-	txt  =`${titles[_type][randum(titles[_type])]} of ${realms[realm][randum(realms[realm])]}`
+	txt  =`${data.titlesData.titles[_type][randum(data.titlesData.titles[_type])]} of ${realms[realm][randum(realms[realm])]}`
 	return txt
 }
 
