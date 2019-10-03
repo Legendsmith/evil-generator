8e396914549e60ada4a316593b44718fcb3b0cc3
A.W. Stanley <aws@sysfunc.net>
2019-10-03 12:03:28 +1000

    Non-root hack added. Moved go server file to subdir that won't resolve on www (gh-pages).

--- app/apps/evil-standalone.js
+++ app/apps/evil-standalone.js
@@ -1,8 +1,18 @@
 // Configuration for standalone evil.
 // (But even never stands alone...)
 
+let hrefSplit = window.location.href.split("//")[1].split("/");
+window.prefix = "/";
+if (hrefSplit.length > 2) {
+    // Determine prefix.
+    window.prefix = window.location.href.split(hrefSplit[0])[1];
+}
+
+console.log(hrefSplit);
+console.log(window.prefix);
+
 requirejs.config({
-    baseUrl: '/app',
+    baseUrl: window.prefix + 'app',
     paths: {
         core: 'core',
         gen: 'gen',
